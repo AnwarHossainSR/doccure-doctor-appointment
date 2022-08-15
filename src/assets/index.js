9e2f387292cdfcd88bf95e02719f4771cc812597
Anwar Hossain <mahedianwar@gmail.com>
2022-08-15 10:58:18 +0600

    design specialities section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,20 @@
 import Logo from './images/logo.png';
+import Speciality1 from './images/specialities/specialities-01.png';
+import Speciality2 from './images/specialities/specialities-02.png';
+import Speciality3 from './images/specialities/specialities-03.png';
+import Speciality4 from './images/specialities/specialities-04.png';
+import Speciality5 from './images/specialities/specialities-05.png';
+
 const assets = {
   images: {
     Logo,
+    specialities: {
+      Speciality1,
+      Speciality2,
+      Speciality3,
+      Speciality4,
+      Speciality5,
+    },
   },
 };
 
