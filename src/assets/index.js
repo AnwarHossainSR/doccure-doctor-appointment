6e4ad2d22c1373c0432a275689c5c1aa31d3cb67
Restyled.io <commits@restyled.io>
2022-08-19 10:24:13 +0000

    Restyled by prettier

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,48 +1,48 @@
-import blog1 from './images/blogs/post1.jpeg';
-import blog1author from './images/blogs/post1_author.jpeg';
-import blog2 from './images/blogs/post2.jpeg';
-import blog2author from './images/blogs/post2_author.jpeg';
-import blog3 from './images/blogs/post3.jpeg';
-import blog3author from './images/blogs/post3_author.jpeg';
-import blog4 from './images/blogs/post4.jpeg';
-import blog4author from './images/blogs/post4_author.jpeg';
-import Doctor1 from './images/doctors/doctor-01.jpg';
-import Doctor2 from './images/doctors/doctor-02.jpg';
-import Doctor3 from './images/doctors/doctor-03.jpg';
-import Doctor4 from './images/doctors/doctor-04.jpg';
-import Doctor5 from './images/doctors/doctor-05.jpg';
-import Doctor6 from './images/doctors/doctor-06.jpg';
-import Doctor7 from './images/doctors/doctor-07.jpg';
-import Doctor8 from './images/doctors/doctor-08.jpg';
-import Doctor9 from './images/doctors/doctor-09.jpg';
-import Doctor10 from './images/doctors/doctor-10.jpg';
-import Doctor11 from './images/doctors/doctor-11.jpg';
-import Doctor12 from './images/doctors/doctor-12.jpg';
-import Feature1 from './images/features/feature-01.jpg';
-import Feature2 from './images/features/feature-02.jpg';
-import Feature3 from './images/features/feature-03.jpg';
-import Feature4 from './images/features/feature-04.jpg';
-import Feature5 from './images/features/feature-05.jpg';
-import Feature6 from './images/features/feature-06.jpg';
-import FeatureIcon from './images/features/feature.png';
-import Logo from './images/logo.png';
-import Speciality1 from './images/specialities/specialities-01.png';
-import Speciality2 from './images/specialities/specialities-02.png';
-import Speciality3 from './images/specialities/specialities-03.png';
-import Speciality4 from './images/specialities/specialities-04.png';
-import Speciality5 from './images/specialities/specialities-05.png';
+import blog1 from "./images/blogs/post1.jpeg";
+import blog1author from "./images/blogs/post1_author.jpeg";
+import blog2 from "./images/blogs/post2.jpeg";
+import blog2author from "./images/blogs/post2_author.jpeg";
+import blog3 from "./images/blogs/post3.jpeg";
+import blog3author from "./images/blogs/post3_author.jpeg";
+import blog4 from "./images/blogs/post4.jpeg";
+import blog4author from "./images/blogs/post4_author.jpeg";
+import Doctor1 from "./images/doctors/doctor-01.jpg";
+import Doctor2 from "./images/doctors/doctor-02.jpg";
+import Doctor3 from "./images/doctors/doctor-03.jpg";
+import Doctor4 from "./images/doctors/doctor-04.jpg";
+import Doctor5 from "./images/doctors/doctor-05.jpg";
+import Doctor6 from "./images/doctors/doctor-06.jpg";
+import Doctor7 from "./images/doctors/doctor-07.jpg";
+import Doctor8 from "./images/doctors/doctor-08.jpg";
+import Doctor9 from "./images/doctors/doctor-09.jpg";
+import Doctor10 from "./images/doctors/doctor-10.jpg";
+import Doctor11 from "./images/doctors/doctor-11.jpg";
+import Doctor12 from "./images/doctors/doctor-12.jpg";
+import Feature1 from "./images/features/feature-01.jpg";
+import Feature2 from "./images/features/feature-02.jpg";
+import Feature3 from "./images/features/feature-03.jpg";
+import Feature4 from "./images/features/feature-04.jpg";
+import Feature5 from "./images/features/feature-05.jpg";
+import Feature6 from "./images/features/feature-06.jpg";
+import FeatureIcon from "./images/features/feature.png";
+import Logo from "./images/logo.png";
+import Speciality1 from "./images/specialities/specialities-01.png";
+import Speciality2 from "./images/specialities/specialities-02.png";
+import Speciality3 from "./images/specialities/specialities-03.png";
+import Speciality4 from "./images/specialities/specialities-04.png";
+import Speciality5 from "./images/specialities/specialities-05.png";
 
 const assets = {
-  images : {
+  images: {
     Logo,
-    specialities : {
+    specialities: {
       Speciality1,
       Speciality2,
       Speciality3,
       Speciality4,
       Speciality5,
     },
-    doctors : {
+    doctors: {
       Doctor1,
       Doctor2,
       Doctor3,
@@ -56,7 +56,7 @@ const assets = {
       Doctor11,
       Doctor12,
     },
-    features : {
+    features: {
       FeatureIcon,
       Feature1,
       Feature2,
@@ -65,7 +65,7 @@ const assets = {
       Feature5,
       Feature6,
     },
-    blogs : {
+    blogs: {
       blog1,
       blog1author,
       blog2,
