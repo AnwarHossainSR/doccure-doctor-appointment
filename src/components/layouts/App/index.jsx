26fd506941513f46652c77ce773a589b9f37a349
Anwar Hossain <mahedianwar@gmail.com>
2022-08-21 22:24:24 +0600

    setup login register page

--- src/components/layouts/App/index.jsx
+++ src/components/layouts/App/index.jsx
@@ -5,6 +5,8 @@ import Doctors from '../../../pages/App/doctors';
 import DoctorProfile from '../../../pages/App/doctors/DoctorProfile';
 import Footer from '../../app/Footer';
 import Navbar from '../../app/Navbar';
+import Registration from '../../../pages/App/auth/Registration';
+import Login from '../../../pages/App/auth/Login';
 const index = () => {
   return (
     <div>
@@ -13,6 +15,8 @@ const index = () => {
         <Routes>
           <Route path='/'>
             <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='login' element={<Registration />} />
             <Route path='doctors'>
               <Route index element={<Doctors />} />
               <Route path=':slug' element={<DoctorProfile />} />
