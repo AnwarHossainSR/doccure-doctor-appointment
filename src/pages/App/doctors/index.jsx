0917906405cd1985780b24c912d54333bd138ee8
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-08-22 13:38:37 +0600

    create doctors page

--- src/pages/App/doctors/index.jsx
+++ src/pages/App/doctors/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const index = () => {
-  return <div>All doctors</div>;
+  return (
+    <div className='section all-doctors'>
+      <div className='container-fluid'>
+        <div className='doctors-header'>
+          <h3>All Doctors</h3>
+          <div className='doctors-sort'>
+            <select>
+              <option>All</option>
+              <option>Rating</option>
+              <option>Popular</option>
+              <option>Latest</option>
+              <option>Free</option>
+            </select>
+          </div>
+        </div>
+        <div className='doctors-body'>Doctors Card</div>
+      </div>
+    </div>
+  );
 };
 
 export default index;
